6708396e7d93e0561d4c0996b6a00c37b528a6c3
Gildon Kirk <gildonkirk@gmail.com>
2017-08-24 11:44:08 -0400

    sequelize is set up

--- server.js
+++ server.js
@@ -14,6 +14,10 @@ const path = require("path");
 
 // Tells node that we are creating an "express" server
 let app = express();
+var passport = require('passport');
+var session = require('express-session');
+var env = require('dotenv').load();
+
 
 // Sets an initial port. We'll use this later in our listener
 const PORT = process.env.PORT || 8080; // This is a local placeholder
@@ -30,14 +34,40 @@ app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 // app.use(bodyParser.raw({ type: 'application/vnd.custom-type' }))
 
+// For Passport
+app.use(session({ secret: 'keyboard cat',resave: true, saveUninitialized:true})); // session secret
+ 
+app.use(passport.initialize());
+ 
+app.use(passport.session()); // persistent login sessions
+
+// Getting an error that says require is not a function, this is because it
+// has been exported as a variable not a function
+// require("./app/routes/apiRoutes")(app);
+// require("./app/routes/htmlRoutes")(app);
 // ================================================================================
+
+//Models
+var models = require("./models");
+ 
+//Sync Database
+models.sequelize.sync().then(function() {
+    console.log('Nice! Database looks fine')
+}).catch(function(err) {
+    console.log(err, "Something went wrong with the Database Update!")
+});
+
+
+
 // ROUTER
 // The below points our server to a series of "route" files.
 // These routes give our server a "map" of how to respond when users visit or request data from various URLs.
 // ================================================================================
+app.get('/', function(req, res) { 
+    res.send('Welcome to Passport with Sequelize');
+});
 
-require("./app/routing/apiRoutes")(app);
-require("./app/routing/htmlRoutes")(app);
+
 
 // ==============================================================================
 // LISTENER
