a0c9d7516396d8af76aef398a7b5944b202d21a3
Gildon Kirk <gildonkirk@gmail.com>
2017-08-24 19:34:00 -0400

    log soemthing

--- models/index.js
+++ models/index.js
@@ -7,6 +7,8 @@ var Sequelize = require('sequelize');
 var env       = process.env.NODE_ENV || 'development';
 var config    = require(__dirname + '/../config/config.json')[env];
 var db        = {};
+console.log('>><>>><');
+console.log(config);
 var sequelize = new Sequelize(config.database, config.username, config.password, config);
 
 // if (config.use_env_variable) {
