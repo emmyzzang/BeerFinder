8b10db6236dd9d88f652441f1dca2766aeadafe8
Gildon Kirk <gildonkirk@gmail.com>
2017-08-24 20:01:36 -0400

    JAWS WORKS!!!!!

--- models/index.js
+++ models/index.js
@@ -7,15 +7,12 @@ var Sequelize = require('sequelize');
 var env       = process.env.NODE_ENV || 'development';
 var config    = require(__dirname + '/../config/config.json')[env];
 var db        = {};
-console.log('>><>>><');
-console.log(config);
-var sequelize = new Sequelize(process.env.JAWSDB_URL);
 
-// if (config.use_env_variable) {
-//   var sequelize = new Sequelize(process.env[config.use_env_variable]);
-// } else {
-//   var sequelize = new Sequelize(config.database, config.username, config.password, config);
-// }
+if (config.use_env_variable) {
+  var sequelize = new Sequelize(process.env[config.use_env_variable]);
+} else {
+  var sequelize = new Sequelize(config.database, config.username, config.password, config);
+}
 
 fs
   .readdirSync(__dirname)
