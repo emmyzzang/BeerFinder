7ea13c3ce749bb7f6cec9a14f81369642c570e55
Gildon Kirk <gildonkirk@gmail.com>
2017-08-29 19:04:08 -0400

    succesfully got a beer from beers api

--- app/routes/apiRoutes.js
+++ app/routes/apiRoutes.js
@@ -11,52 +11,60 @@
 // // ===============================================================================
 // // ROUTING
 // // ===============================================================================
+var axios = require('axios');
+var router = require('express').Router();
 
-// module.exports = function(app) {
-//   // API GET Requests
-//   // Below code handles when users "visit" a page.
-//   // In each of the below cases when a user visits a link
-//   // (ex: localhost:PORT/api/admin... they are shown a JSON of the data)
-//   // ---------------------------------------------------------------------------
+  // API GET Requests
+  // Below code handles when users "visit" a page.
+  // In each of the below cases when a user visits a link
+  // (ex: localhost:PORT/api/admin... they are shown a JSON of the data)
+  // ---------------------------------------------------------------------------
 
-//   app.get("/api/beers", function(req, res) {
-//     res.json(tableData);
+  router.get("/beers", function(req, res) {
+  	console.log('getting beers!')
+  	axios.get('http://api.brewerydb.com/v2/beers?styleId=15&key=3f1612c064ffbdbd5925f006fa955076')
+  		.then(function(response) {
+  			console.log(response.data);
+  			res.json(response.data);
+  		})
+    // res.json(tableData);
+  });
+
+  // app.get("/api/beerAdd", function(req, res) {
+  //   res.json(waitListData);
+  // });
+
+  // API POST Requests
+  // Below code handles when a user submits a form and thus submits data to the server.
+  // In each of the below cases, when a user submits form data (a JSON object)
+  // ...the JSON is pushed to the appropriate JS array
+  // (ex. User fills out a beer [_________]... this data is then sent to the server...
+  // Then the server saves the data to the beerData array)
+  // ---------------------------------------------------------------------------
+
+  // app.post("/api/beers", function(req, res) {
+  //   // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
+  //   // It will do this by sending out the value "true" have a beer
+  //   if (beerData.length < ___[PLACEHOLDER]) {
+  //     beerData.push(req.body);
+  //     res.json(true);
+  //   }
+  //   else {
+  //     beerAddData.push(req.body);
+  //     res.json(false);
+  //   }
+  // });
+
+  // ---------------------------------------------------------------------------
+// I added this below code so you could clear out the beerData while working with the functionality.
+//   // Don't worry too much about it!
+
+//   app.post("/api/clear", function() {
+//     // Empty out the arrays of data
+//     beerData = [];
+//     beerAddData = [];
+
+//     console.log(beerData);
 //   });
 
-//   app.get("/api/beerAdd", function(req, res) {
-//     res.json(waitListData);
-//   });
-
-//   // API POST Requests
-//   // Below code handles when a user submits a form and thus submits data to the server.
-//   // In each of the below cases, when a user submits form data (a JSON object)
-//   // ...the JSON is pushed to the appropriate JS array
-//   // (ex. User fills out a beer [_________]... this data is then sent to the server...
-//   // Then the server saves the data to the beerData array)
-//   // ---------------------------------------------------------------------------
-
-//   app.post("/api/beers", function(req, res) {
-//     // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
-//     // It will do this by sending out the value "true" have a beer
-//     if (beerData.length < ___[PLACEHOLDER]) {
-//       beerData.push(req.body);
-//       res.json(true);
-//     }
-//     else {
-//       beerAddData.push(req.body);
-//       res.json(false);
-//     }
-//   });
-
-//   // ---------------------------------------------------------------------------
-// // I added this below code so you could clear out the beerData while working with the functionality.
-// //   // Don't worry too much about it!
-
-// //   app.post("/api/clear", function() {
-// //     // Empty out the arrays of data
-// //     beerData = [];
-// //     beerAddData = [];
-
-// //     console.log(beerData);
-// //   });
-// };
+module.exports = router;
